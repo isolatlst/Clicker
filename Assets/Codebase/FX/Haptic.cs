823fe0872d20bba36cdf311e6225e7aabde25cbf
Kirill <kirill.huryn.unity.dev@gmail.com>
2025-03-04 03:45:04 +0300

    Add-haptic

--- Assets/Codebase/FX/Haptic.cs
+++ Assets/Codebase/FX/Haptic.cs
@@ -1,15 +1,40 @@
-﻿using UnityEngine;
+﻿using CandyCoded.HapticFeedback;
+using Codebase.Infrastructure;
+using Codebase.Infrastructure.Signals.Enemy;
+using UnityEngine;
 
 namespace Codebase.FX
 {
     public class Haptic : MonoBehaviour, IFxControlable
     {
+        private bool _isShouldPlay;
+
+        private void Awake()
+        {
+            SignalBus.Subscribe<EnemyDeathSignal>(PlayEffect);
+        }
+
+        private void PlayEffect(EnemyDeathSignal signal)
+        {
+            if (_isShouldPlay)
+            {
+                HapticFeedback.LightFeedback();
+            }
+        }
+
         public void On()
         {
+            _isShouldPlay = true;
         }
 
         public void Off()
         {
+            _isShouldPlay = false;
+        }
+
+        private void OnDestroy()
+        {
+            SignalBus.Unsubscribe<EnemyDeathSignal>(PlayEffect);
         }
     }
 }
